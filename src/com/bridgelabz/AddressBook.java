77ed78314c862d0f207fbdf64a1795a61ecfaaf6
vanibm15 <vanibm15@gmail.com>
2022-05-27 19:04:26 +0530

    added:new contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -5,24 +5,21 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 public class AddressBook {
+    List<Contact> contactList = new ArrayList<>();
+
+    void addNewContact() {
+        Contact contact = new Contact();
+        contact.addContact();
+        contactList.add(contact);
+        contact.displayContact();
+
+
+    }
+
     public static void main(String[] args) {
-        Contact contact1=new Contact("vani","bm","hubli karnataka","vanibm",987456321,236 );
-//        System.out.println(contact.getFirstName());
-//        System.out.println(contact.getLastName());
-//        System.out.println(contact.getAddress());
-//        System.out.println(contact.getEmail());
-//        System.out.println(contact.getPhNo());
-//        System.out.println(contact.getZip());
-Contact contact2=new Contact("bbb","mm","mmm","hgh",56451,564);
-        List<Contact>contacts=new ArrayList<>();
-        contacts.add(contact1 );
-        contacts.add(contact2);
-        System.out.println(contacts  );
+        AddressBook addressBook = new AddressBook();
+        addressBook.addNewContact();
 
 
-
-
-            }
+    }
 }
-
-
