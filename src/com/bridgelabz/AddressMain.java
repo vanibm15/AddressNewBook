c48b5412ee6ef46197a0063bbb420b978daf3c66
vanibm15 <vanibm15@gmail.com>
2022-05-29 16:47:00 +0530

    added:delete person

---
+++ src/com/bridgelabz/AddressMain.java
@@ -0,0 +1,87 @@
+package com.bridgelabz;
+
+import com.bridgelabz.Contacts;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+publ class AddressMain {
+
+    static Map<String, Contacts> contactsMap = new HashMap<>();
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static void addNewContact() {
+        Contacts contacts = new Contacts();
+        contacts.addContact();
+        contactsMap.put(contacts.getPhoneNumber(), contacts);
+    }
+
+    private static void editContact() {
+        System.out.println("Enter the contactNumber");
+        String phoneNumber = scanner.next();
+        Contacts contacts = contactsMap.get(phoneNumber);
+        if (contacts != null) {
+            contacts.addContact();
+        } else {
+            System.out.println("The contact is not present in Addressbook");
+        }
+    }
+
+    private static void showContact() {
+        System.out.println("Enter the contactNumber");
+        String phoneNumber = scanner.next();
+        Contacts contacts = contactsMap.get(phoneNumber);
+        if (contacts != null) {
+            contacts.displayContact();
+        } else {
+            System.out.println("The contact is not present in AddressBook");
+        }
+    }
+
+    private static void deleteContact() {
+        System.out.println("Enter the ContactNumber");
+        String phoneNumber = scanner.next();
+        Contacts contacts = contactsMap.get(phoneNumber);
+        if (contacts != null) {
+            contactsMap.remove(contacts);
+        } else {
+            System.out.println("The contact is not present in Addressbook");
+        }
+    }
+
+    static void contactOperations() {
+        int choice;
+        do {
+            System.out.println("Enter the choice");
+            System.out.println("1:ADD CONTACT");
+            System.out.println("2:EDIT CONTACT");
+            System.out.println("3:DISPLAY CONTACT");
+            System.out.println("4:DELETE CONTACT");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addNewContact();
+                    break;
+                case 2:
+                    editContact();
+                    break;
+                case 3:
+                    showContact();
+                    break;
+                case 4:
+                    deleteContact();
+                    break;
+                default:
+                    System.out.println("Enter the choices between 1-5");
+                    contactOperations();
+            }
+        } while (choice != 5);
+    }
+
+    public static void main(String[] args) {
+        AddressMain addressBook = new AddressMain();
+        contactOperations();
+    }
+}
---
+++ src/com/bridgelabz/Contacts.java
@@ -0,0 +1,144 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Contacts {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private int zip;
+    private String state;
+    private String city;
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public Contacts() {
+    }
+
+    public Contacts(String firstname, String lastname, String email,
+                    String phoneNumber, String address, int zip, String state, String city) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.zip = zip;
+        this.state = state;
+        this.city = city;
+    }
+
+    public void addContact() {
+        System.out.println("Add the contact");
+        int numberOfContacts = scanner.nextInt();
+        for (int i = 0; i < numberOfContacts; i++) {
+            System.out.println("Enter the details of contact number " + (i + 1));
+            System.out.println("Enter the firstName");
+            firstname = scanner.next();
+            System.out.println("Enter the Lastname");
+            lastname = scanner.next();
+            System.out.println("Enter the Email");
+            email = scanner.next();
+            System.out.println("Enter the phonenumber");
+            phoneNumber = scanner.next();
+            System.out.println("Enter the Address");
+            address = scanner.next();
+            System.out.println("Enter the Zip");
+            zip = scanner.nextInt();
+            System.out.println("Enter the state");
+            state = scanner.next();
+            System.out.println("Enter the city");
+            city = scanner.next();
+            System.out.println("Contact number " + (i + 1) + " saved");
+            System.out.println("==================================");
+            Contacts contacts1 = new Contacts(firstname, lastname, email,
+                    phoneNumber, address, zip, state, city);
+            AddressMain.contactsMap.put(contacts1.getPhoneNumber(), contacts1);
+        }
+    }
+    void displayContact() {
+        System.out.println("FirstName:" + firstname + "\n" + "Lastname:"
+                + lastname + "\n" + "Phonenumber:" + "\n" + phoneNumber + "Address:" + address + "\n"
+                + "zip:" + zip + "\n" + "state:" + state + "\n" + "city" + city);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacts{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                ", zip=" + zip +
+                ", state='" + state + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
